15fcb79d6bb1efd0fc3ebd58de8300b883c0325a
Reuben Tong <reuben.tong@hotmail.com>
2022-10-04 12:26:16 +0100

    feat: upd

--- python/python-leetcode/time_to_make_rope_colorful.py
+++ python/python-leetcode/time_to_make_rope_colorful.py
@@ -1,1 +1,20 @@
-#https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+# https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+# https://leetcode.com/submissions/detail/814948749/
+from typing import List
+
+
+# loop through str, if != one after ignore,
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        time = 0
+        for i in range(len(colors) - 1):
+            if colors[i] != colors[i + 1]:
+                continue
+            if neededTime[i] > neededTime[i + 1]:
+                neededTime[i], neededTime[i + 1] = neededTime[i + 1], neededTime[i]
+            time += neededTime[i]
+        return time
+
+
+sol = Solution()
+print(sol.minCost("abbabba", [1, 2, 3, 2, 6, 7, 2]))
